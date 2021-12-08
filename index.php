2b49e8f7c6b960b8b07d5dfafb8cfe737f6f69e5
janekimeu <95747999+janekimeu@users.noreply.github.com>
2021-12-08 13:19:39 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME PAGE</title>
+</head>
+<body>
+
+
+
+
+<div class="container-fluid">
+      <div class="row-fluid">
+	<div class="span2">
+          <div class="well sidebar-nav">
+                     <ul class="nav nav-list" style="font: 10px ">
+              <li class="active"><a href="#"><i class="icon-dashboard icon-2x"></i> Dashboard </a></li> <br>
+			<li><a href="sales.php?id=cash&invoice=<?php echo $finalcode ?>"><i class="icon-shopping-cart icon-2x"></i> Sales</a>  </li>             
+			<li><a href="products.php"><i class="icon-list-alt icon-2x"></i> Products</a><br>                                     </li>
+			<li><a href="customer.php"><i class="icon-group icon-2x"></i> Customers</a><br>                                    </li>
+			<li><a href="supplier.php"><i class="icon-group icon-2x"></i> Suppliers</a><br>                                    </li>
+			<li><a href="salesreport.php?d1=0&d2=0"><i class="icon-bar-chart icon-2x"></i> Sales Report</a>                </li>
+			
+		
+			  </div>
+			</li>
+				</ul>                               
+          </div><!--/.well -->
+        </div><!--/span-->
+	
+
+
+
+</body>
+</html>
